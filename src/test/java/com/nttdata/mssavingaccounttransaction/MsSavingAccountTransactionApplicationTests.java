54a122857506cb2aa88133f55a668cfbacd86172
Mauricio Kevin Sulca Chavez <msulccha@emeal.nttdata.com>
2022-08-18 18:53:36 -0500

    primer

---
+++ src/test/java/com/nttdata/mssavingaccounttransaction/MsSavingAccountTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.mssavingaccounttransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSavingAccountTransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
